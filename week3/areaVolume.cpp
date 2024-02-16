adaf64b30c460da3cd0d9c8e82967140dbfb3f7b
sai1505 <86135662+sai1505@users.noreply.github.com>
2024-02-16 16:28:59 +0530

    Create areaVolume.cpp

---
+++ week3/areaVolume.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include "areaBox.h"
+#include "volumeBox.h"
+using namespace std;
+
+int main()
+{
+  float length,breadth,height;
+  cout<<"Enter the length, breadth, and height : ";
+  cin>>length>>breadth>>height;
+  #ifdef AREA
+  {
+    area(length,breadth,height);
+  }
+  #endif
+  #ifndef AREA
+  {
+    cout<<"You have not included areaBox.h file"<<endl;
+  }
+  #endif
+  #ifdef VOLUME
+  {
+    volume(length,breadth,height);
+  }
+  #endif
+  #ifndef VOLUME
+  {
+    cout<<"You have not included volume.h file"<<endl;
+  }
+  #endif
+  return 0;
+}
