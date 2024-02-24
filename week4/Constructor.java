2c0e0330d96c9859bfb2d038c9a1b5bff85c1ba8
sai1505 <86135662+sai1505@users.noreply.github.com>
2024-02-24 12:32:34 +0530

    Update Constructor.java

--- week4/Constructor.java
+++ week4/Constructor.java
@@ -1,31 +1,25 @@
-/*Using a constructor and destructor in Java, Write a Java class 'Student' and assign the following :
-1.default constructor (string collegeName = "MVGR", int collegeCode=33)
-2.parameterized constructor (string fullName, double semPerentage)
-Also create the objects respectivelty asd display their values.*/
 class Student
 {
-    public
-        Student()
-        {
-            String collegeName="MVGR";
-            int collegeCode=33;
-            System.out.print("CollegeName : "+collegeName+"\n");
-            System.out.print("CollegeCode : "+collegeCode+"\n");
-        }
-        Student(String fullName, double semPercentage)
-        {
-            System.out.print("fullName : "+fullName+"\n");
-            System.out.print("semPercentage: "+semPercentage+"\n");
-        }
+  public
+    Student()
+    {
+      String collegeName="MVGR";
+      int collegeCode=33;
+      System.out.print("CollegeName : "+collegeName+"\n"+"CollegeCode : "+collegeCode+"\n");
+    }
+    Student(String fullName,double semPercentage)
+    {
+      System.out.print("FullName : "+fullName+"\n"+"SemPercentage : "+semPercentage);
+    }
 }
 
 public class Constructor
 {
-    public static void main(String[] args)
-    {
-        @SuppressWarnings("unused")
-        Student venkat=new Student();
-        @SuppressWarnings("unused")
-        Student venkat01=new Student("Sai Venkat",89);
-    }
+  public static void main(String[] args)
+  {
+    @SuppressWarnings("unused")
+    Student vanitha=new Student();
+    @SuppressWarnings("unused")
+    Student vanitha01=new Student("G.Vanitha",98);
+  }
 }
