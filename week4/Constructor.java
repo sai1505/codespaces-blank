d67596d1d8690c2aab408f783be931eee1449d9a
sai1505 <86135662+sai1505@users.noreply.github.com>
2024-02-24 18:28:24 +0530

    Update Constructor.java

--- week4/Constructor.java
+++ week4/Constructor.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 class Student
 {
   public
@@ -19,7 +20,12 @@ public class Constructor
   {
     @SuppressWarnings("unused")
     Student venkat=new Student();
+    Scanner input=new Scanner(System.in);
+    String fullName=input.nextLine();
+    double semPercentage=input.nextDouble();
+    input.close();
     @SuppressWarnings("unused")
-    Student venkat01=new Student("B.SaiVenkat",92);
+    Student venkat01=new Student(fullName,semPercentage);
+
   }
 }
