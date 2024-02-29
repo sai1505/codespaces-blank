0623a5206e8468dc89ae41a66b3e5925ce395834
sai1505 <86135662+sai1505@users.noreply.github.com>
2024-02-29 09:58:05 +0530

    Create InheritanceAccess.cpp

---
+++ week5/InheritanceAccess.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+using namespace std;
+
+class SuperClass
+{
+    public:
+        int publicVar=1;
+
+    private:
+        int privateVar=2;
+
+    protected:
+        int protectedVar=3;
+
+    public:
+        int gValp()
+        {
+            return privateVar;
+        }
+        int gValpr()
+        {
+            return protectedVar;
+        }
+        int gValpu()
+        {
+            return publicVar;
+        }
+};
+
+class SubClass01: public SuperClass
+{
+    public:
+        int var1=publicVar;
+        int var2=protectedVar;
+        int var3=gValp();
+};
+
+class SubClass02: private SuperClass
+{
+    public:
+        int var01=gValpu();
+        int var02=gValpr();
+        int var03=gValp();
+};
+
+class SubClass03: protected SuperClass
+{
+    public:
+        int var001=publicVar;
+        int var002=protectedVar;
+        int var003=gValp();
+};
+
+int main()
+{
+    SubClass01 obj1;
+    SubClass02 obj2;
+    SubClass03 obj3;
+    cout<<"SubClass01\n";
+    cout<<"Public : "<<obj1.var1<<endl;
+    cout<<"Protected : "<<obj1.var2<<endl;
+    cout<<"Private : "<<obj1.var3<<endl;
+    cout<<"SubClass02\n";
+    cout<<"Public : "<<obj2.var01<<endl;
+    cout<<"Protected : "<<obj2.var02<<endl;
+    cout<<"Private : "<<obj2.var03<<endl;
+    cout<<"SubClass03\n";
+    cout<<"Public : "<<obj3.var001<<endl;
+    cout<<"Protected : "<<obj3.var002<<endl;
+    cout<<"Private : "<<obj3.var003<<endl;
+    return 0;
+}
