9dfa2e3a3a2d0cf749b8516c53b16e01342895f2
sai1505 <86135662+sai1505@users.noreply.github.com>
2024-04-18 20:58:32 +0530

    Update throwsThrowable.java

--- week9/throwsThrowable.java
+++ week9/throwsThrowable.java
@@ -9,16 +9,15 @@ public class throwsThrowable {
     public static void main(String[] args) throws GroceryException{
         int groceryItems=10;
         int groceryTaken=15;
-        try{
         if(groceryItems==groceryTaken){
             System.out.println("The count of the grocery items are proper.");
         }
         else{
-            throw new GroceryException();
-        }
-        }catch(Exception e){
-            System.out.println("An exception has been occurred.");
-            e.printStackTrace();
+            try{
+                throw new GroceryException();
+            }catch(GroceryException e){
+                //any statements u wanna print.
+            }
         }
     }
 }
