34cb9870a0a18c2df9478e0a3e05907414e1f8ca
sai1505 <86135662+sai1505@users.noreply.github.com>
2024-04-29 05:25:05 +0530

    Add files via upload

---
+++ week10/Demo.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import java.applet.*;
+
+/*
+<applet code="Demo.class" width="1000" height="1000"> </applet>
+*/
+public class Demo extends Applet {
+
+	public void init(){
+		
+	}
+	
+	public void paint(Graphics g) {
+		g.drawLine(10,10,300,300);
+	}
+}
