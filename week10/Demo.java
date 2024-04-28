8b0fb24c21cf8f6d4a5b421c8d877c77391c6b78
sai1505 <86135662+sai1505@users.noreply.github.com>
2024-04-29 05:27:16 +0530

    Update Demo.java

--- week10/Demo.java
+++ week10/Demo.java
@@ -5,10 +5,6 @@ import java.applet.*;
 <applet code="Demo.class" width="1000" height="1000"> </applet>
 */
 public class Demo extends Applet {
-
-	public void init(){
-		
-	}
 	
 	public void paint(Graphics g) {
 		g.drawLine(10,10,300,300);
